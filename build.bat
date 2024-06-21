408621d87d6f0e4054f99378b57111409f139a82
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2024-06-21 23:31:22 +0300

    Fix pedantic errors on , with va_args on log

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@ rem @echo off
 @setlocal
 
 rem set compiler=tcc.exe
-set compiler=clang.exe -O2
-rem set compiler=clang.exe  -g -Wpedantic
+rem set compiler=clang.exe -O2
+set compiler=clang.exe  -g -Wpedantic
 
 set links=-lopengl32 -lgdi32 -luser32 -lwinmm -m64 -std=c11
 set linkfolders=-Lc:/windows/SysWOW64 -I./
@@ -15,8 +15,9 @@ set compilesource=examples\hotreload.c
 md buildout
 
 
-clang mathtest.c -O2  -g -obuildout/mathtestclang.exe %links% && buildout\mathtestclang.exe > buildout/mathtestclang.txt
-tcc mathtest.c -O2 -g -obuildout/mathtesttcc.exe %final% && buildout\mathtesttcc.exe > buildout/mathtesttcc.txt
+rem clang mathtest.c -O2  -g -obuildout/mathtestclang.exe %links% && buildout\mathtestclang.exe > buildout/mathtestclang.txt
+rem tcc mathtest.c -O2 -g -obuildout/mathtesttcc.exe %final% && buildout\mathtesttcc.exe > buildout/mathtesttcc.txt
+
 rem tcc mathtest.c -O2 -g -obuildout/mathtesttcc.exe %final% && buildout\mathtesttcc.exe
 
 %compiler% %compilesource% -obuildout/main.exe %final% && buildout\main.exe
--- carplib/carplog.h
+++ carplib/carplog.h
@@ -6,17 +6,28 @@
 #define CARP_USE_LOG 1
 #if CARP_USE_LOG == 1
 
-#define CARP_HELPER_LOG(LOG_TEXT_STRING, ...) \
+//#define CARP_HELPER_LOG(LOG_TEXT_STRING, ...) \
+//                do { \
+//                    fprintf(stdout, LOG_TEXT_STRING, __VA_ARGS__); \
+//                } while(0)
+
+#define CARP_HELPER_LOG(...) \
                 do { \
-                    printf(LOG_TEXT_STRING, ##__VA_ARGS__); \
+                    fprintf(__VA_ARGS__); \
                 } while(0)
 #else
-#define HELPER_LOG(LOG_TEXT_STRING, ...)
+//#define HELPER_LOG(LOG_TEXT_STRING, ...)
+#define HELPER_LOG(...)
 #endif // LOGDEBUG
 
-#define CARP_LOG(LOG_TEXT_STRING, ...) CARP_HELPER_LOG(LOG_TEXT_STRING, ##__VA_ARGS__)
-#define CARP_LOGINFO(LOG_TEXT_STRING, ...) CARP_HELPER_LOG("Info: " LOG_TEXT_STRING , ##__VA_ARGS__)
-#define CARP_LOGDEBUG(LOG_TEXT_STRING, ...) CARP_HELPER_LOG("Debug: " LOG_TEXT_STRING, ##__VA_ARGS__)
-#define CARP_LOGERROR(LOG_TEXT_STRING, ...) CARP_HELPER_LOG("Error: " LOG_TEXT_STRING, ##__VA_ARGS__)
+//#define CARP_LOG(LOG_TEXT_STRING, ...) CARP_HELPER_LOG(LOG_TEXT_STRING, ##__VA_ARGS__)
+//#define CARP_LOGINFO(LOG_TEXT_STRING, ...) CARP_HELPER_LOG("Info: " LOG_TEXT_STRING , ##__VA_ARGS__)
+//#define CARP_LOGDEBUG(LOG_TEXT_STRING, ...) CARP_HELPER_LOG("Debug: " LOG_TEXT_STRING, ##__VA_ARGS__)
+//#define CARP_LOGERROR(LOG_TEXT_STRING, ...) CARP_HELPER_LOG("Error: " LOG_TEXT_STRING, ##__VA_ARGS__)
+
+#define CARP_LOG(...) CARP_HELPER_LOG(stdout, __VA_ARGS__)
+#define CARP_LOGINFO(...) CARP_HELPER_LOG(stdout, "Info: " __VA_ARGS__)
+#define CARP_LOGDEBUG(...) CARP_HELPER_LOG(stdout, "Debug: " __VA_ARGS__)
+#define CARP_LOGERROR(...) CARP_HELPER_LOG(stderr, "Error: " __VA_ARGS__)
 
 #endif //CARPLOG_HH
